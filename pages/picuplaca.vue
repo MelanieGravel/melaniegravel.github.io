4fac913466e57eaa184d8a5453aac836544d38bc
Melanie Gravel <melanie.gravel@outlook.com>
2021-01-06 22:23:50 -0500

    Routing

---
+++ pages/picuplaca.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>PICU PLACA</h1>
+  </div>
+</template>
+
+<script lang="ts">
+// Picu Placa
+// EVEN Plate - ODD DATES
+// ODD Plate - EVEN DATES
+// Monday to Friday
+
+// Confinement ID
+// EVEN ID - ODD DATES
+// ODD ID - EVEN DATES
+// 7 days
+</script>
+
+<style lang="scss"></style>
