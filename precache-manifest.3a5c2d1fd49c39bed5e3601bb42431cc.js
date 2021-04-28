46d79630172bd69d5457bbf0b3762a4625466606
MelanieGravel <melanie.gravel@outlook.com>
2021-04-28 17:45:37 +0000

    gh-pages

---
+++ precache-manifest.3a5c2d1fd49c39bed5e3601bb42431cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b9a8f65d7da9076892",
+    "url": "/css/chunk-vendors.9597c947.css"
+  },
+  {
+    "revision": "b3adb05760e2e4f41aa32067c98a6f69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab87d0e67aa0af044820",
+    "url": "/js/about.f1f8c00a.js"
+  },
+  {
+    "revision": "3b79d1112c4aeb89ab94",
+    "url": "/js/app.301e15f9.js"
+  },
+  {
+    "revision": "d4b9a8f65d7da9076892",
+    "url": "/js/chunk-vendors.62fc6a0d.js"
+  },
+  {
+    "revision": "14cf8759dc63a68ac094ed4e1a85d4dd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
